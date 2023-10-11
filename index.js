73dd8e980680833759fc3598f80d72167d319dc6
SagarK1999 <55268435+SagarK1999@users.noreply.github.com>
2023-10-11 18:14:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 
 async function runExample() {
 
-  var x = new Float32Array(1, 11)
+    var x = new Float32Array( 1, 11 )
 
-  var x = [];
-  
-  x[0] = document.getElementById('box1').value;
-  x[1] = document.getElementById('box2').value;
-  x[2] = document.getElementById('box3').value;
-  x[3] = document.getElementById('box4').value;
-  x[4] = document.getElementById('box5').value;
-  x[5] = document.getElementById('box6').value;
-  x[6] = document.getElementById('box7').value;
-  x[7] = document.getElementById('box8').value;
-  x[8] = document.getElementById('box9').value;
-  x[9] = document.getElementById('box10').value;
-  x[10] = document.getElementById('box11').value;
+    var x = [];
 
-  let tensorx = new onnx.Tensor(x, 'float32', [1, 11])      
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+     x[4] = document.getElementById('box5').value;
+     x[5] = document.getElementById('box6').value;
+     x[6] = document.getElementById('box7').value;
+     x[7] = document.getElementById('box8').value;
+     x[8] = document.getElementById('box9').value;
+     x[9] = document.getElementById('box10').value;
+     x[10] = document.getElementById('box11').value;
 
-  let session = new onnx.InferenceSession();
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
 
-  await session.loadModel("./DLnet_WineData.onnx");
-  let outputMap = await sesssion.run([tensorX]);
-  let outputData = outputMap.get('output1')
+    let session = new onnx.InferenceSession();
 
-  let predictions = document.getElementById('predictions');
+    await session.loadModel("./DLnet_WineData.onnx");
+    let outputMap = await session.run([tensorX]);
+    let outputData = outputMap.get('output1');
+
+   let predictions = document.getElementById('predictions');
 
   predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-  <table>
-    <tr>
-      <td> Rating of Wine Quality </td>
-      <td id="td0"> ${outputData.data[0].toFixed(2)} </td>
-    </tr>
+   <table>
+     <tr>
+       <td>  Rating of Wine Quality  </td>
+       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
+     </tr>
   </table>`;
+    
 
-}  
+
+}
